28a49f4ca915188f794e26f00725302841ae9da1
Chloe Foronda <caforonda103@yahoo.com>
2019-12-23 03:36:33 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,7 +73,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     programmer_hash.push :yukihiro_matsumoto => {
+     programmer_hash[:yukihiro_matsumoto => {
           :known_for => "Ruby",
           :languages => ["LISP", "C"]
      }
